5f9cb3d379f327c32f8620a58b4d565b8b2c517f
Alan215 <alan215m@gmail.com>
2016-06-27 16:15:03 +0800

    Test.

--- py_threading.py
+++ py_threading.py
@@ -1,25 +1,32 @@
+#refer:http://www.cnblogs.com/fnng/p/3670789.html
+
+#coding=utf-8
 import threading
-import time
+from time import ctime,sleep
 
 
-def printf(i):
-    for x in xrange(5):
-        time.sleep(1)
-        print i,
+def music(func):
+    for i in range(2):
+        print "I was listening to %s. %s" %(func,ctime())
+        sleep(10)
 
-def test():
-    thread_list = []
-    for i in xrange(10):
-        sthread = threading.Thread(target = printf, args = str(i))
-#        sthread.setDaemon(True)
-        sthread.start()
-        thread_list.append(sthread)
-#    for i in xrange(10):
-#        thread_list[i].join()
+def move(func):
+    for i in range(2):
+        print "I was at the %s! %s" %(func,ctime())
+        sleep(5)
 
+threads = []
+t1 = threading.Thread(target=music,args=(u'爱情买卖',))
+threads.append(t1)
+t2 = threading.Thread(target=move,args=(u'阿凡达',))
+threads.append(t2)
 
 if __name__ == '__main__':
-    test()
+    for t in threads:
+        t.setDaemon(True)
+        #setDaemon(True)将线程声明为守护线程，必须在start() 方法调用之前设置，如果不设置为守护线程程序会被无限挂起。子线程启动后，父线程也继续执行下去，当父线程执行完最后一条语句print "all over %s" %ctime()后，没有等待子线程，直接就退出了，同时子线程也一同结束。
+        print '11'
+        t.start()
+    t.join()    #join（）的作用是，在子线程完成运行之前，这个子线程的父线程将一直被阻塞。
 
-
-#Here I am
+    print "all over %s" %ctime()
